0d8673798fede1f42487c9e73b2067d28c64c664
Erika Diaz <ediazbuceta@gmail.com>
2021-07-14 10:43:55 -0500

    Bonus 2 de la tarea terminado

--- datos.js
+++ datos.js
@@ -1,4 +1,4 @@
-/*let marcaAuto = "Mercedes-Benz"  // variable que almacena la marca de un automovil
+/*let marcaAuto = "Mercedes-Benz";  // variable que almacena la marca de un automovil
 let edad = 24;                   // variable que almacena la edad
 let hacker1 = "Erika"            //variable que almacena el nombre del conductor
 
@@ -17,10 +17,10 @@ mascota = window.prompt("Que mascota tiene?","");
 console.log("Hola, yo soy " + nombre + ", mis hobbies son " + hobby + ". Me gusta " + gusto + ". Tengo " + mascota + " como mascota." );
 
 //Tarjeta de presentacion con los valores ya definidos
-let nombre2 = "Erika"
-let hobby2 = "ver peliculas de accion, jugar con mi sobrinito e investigar sobre grupos musicales"
-let gusto2 = "La musica, el arte y dormir"
-let mascota2 = "dos perritos"
+let nombre2 = "Erika";
+let hobby2 = "ver peliculas de accion, jugar con mi sobrinito e investigar sobre grupos musicales";
+let gusto2 = "La musica, el arte y dormir";
+let mascota2 = "dos perritos";
 console.log("Hola, yo soy " + nombre2 + ", mis hobbies son " + hobby2 +". Me gusta " + gusto2 + ". Tengo " + mascota2 + " como mascotas.");*/
 
 
