4d84e1d52bea9d5b55fa698a5107151a47cd05ae
enrique <ramirezgalindoenrique@gmail.com>
2021-07-14 10:04:04 -0500

    done

---
+++ js/index.js
@@ -0,0 +1,132 @@
+let marcaAuto = "Mitsubishi";   // Variable con marca de automovil
+
+let edad;   // Variable para almacenar la edad
+
+let hacker1 = "Enrique";    // Variable con nombre del conductor
+
+console.log("El nombre del conductor es: " + hacker1);  // Imprime el nombre del conductor
+
+let numUno = 38;
+let numDos = 14;
+console.log("numUno + numDos = "+ (numUno+numDos)); // Imprime suma de variables numUno + numDos
+
+
+// TARJETA DE PRESENTACION
+let nombre = "Enrique";
+let hobbie = "Programar";
+let mascota = "perro";
+
+console.log("Hola soy " + nombre + ". Me gusta " + hobbie + " en mis tiempos libres y tengo un " + mascota +" como mascota.");
+// Termina tarjeta de presentacion
+
+// CONVERSION TIPOS DE DATOS
+let valor = true;
+console.log("Tipo de dato true = " + typeof(valor));
+valor = String(valor);
+console.log(typeof(valor));
+valor = Number(valor)
+console.log(typeof(valor));
+
+valor = false;
+console.log("Tipo de dato false = " + typeof(valor));
+valor = String(valor);
+console.log(typeof(valor));
+valor = Number(valor)
+console.log(typeof(valor));
+
+valor = 0;
+console.log("Tipo de dato 0 = " + typeof(valor));
+valor = String(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = 1;
+console.log("Tipo de dato 1 = " + typeof(valor));
+valor = String(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = "0";
+console.log("Tipo de dato 0 = " + typeof(valor));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = "000";
+console.log("Tipo de dato 000 = " + typeof(valor));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = "1";
+console.log("Tipo de dato 1 = " + typeof(valor));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = parseInt("parangaricutirimicuaro", 10);
+console.log("Tipo de dato valor = " + parseInt("parangaricutirimicuaro", 10));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = String(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = 1/0;
+console.log("Tipo de dato 1/0 = " + (1/0));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = String(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = -1/0;
+console.log("Tipo de dato 1/0 = " + (-1/0));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = String(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = "";
+console.log("Tipo de dato  = " + typeof(valor));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = "20";
+console.log("Tipo de dato  = " + typeof(valor));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = "twenty";
+console.log("Tipo de dato  = " + typeof(valor));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = null;
+console.log("Tipo de dato  = " + typeof(valor));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
+
+valor = undefined;
+console.log("Tipo de dato  = " + typeof(valor));
+valor = Number(valor);
+console.log(typeof(valor));
+valor = Boolean(valor);
+console.log(typeof(valor));
