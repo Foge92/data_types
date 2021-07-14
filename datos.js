8caed0535a35f9fdca51b37a9d26281b2862aad3
Erika Diaz <ediazbuceta@gmail.com>
2021-07-14 00:25:49 -0500

    Principio de la segunda parte de la tarea

--- datos.js
+++ datos.js
@@ -1,4 +1,4 @@
-let marcaAuto = "Mercedes-Benz"  // variable que almacena la marca de un automovil
+/*let marcaAuto = "Mercedes-Benz"  // variable que almacena la marca de un automovil
 let edad = 24;                   // variable que almacena la edad
 let hacker1 = "Erika"            //variable que almacena el nombre del conductor
 
@@ -21,6 +21,33 @@ let nombre2 = "Erika"
 let hobby2 = "ver peliculas de accion, jugar con mi sobrinito e investigar sobre grupos musicales"
 let gusto2 = "La musica, el arte y dormir"
 let mascota2 = "dos perritos"
-console.log("Hola, yo soy " + nombre2 + ", mis hobbies son " + hobby2 +". Me gusta " + gusto2 + ". Tengo " + mascota2 + " como mascotas.");
+console.log("Hola, yo soy " + nombre2 + ", mis hobbies son " + hobby2 +". Me gusta " + gusto2 + ". Tengo " + mascota2 + " como mascotas.");*/
+
+
+
+let value = false; // Definiendo la variable false 
+console.log("typeof value [" + typeof(value) + "]"); // evaluando que tipo de dato es la variable false
+value = String(value);  // cambiando la variable false a tipo de dato String 
+console.log("typeof value [" + typeof(value) + "]");
+value = Number(value);  // cambiando la variable false a tipo de dato Number
+console.log("typeof value [" + typeof(value) + "]");
+
+let value2 = true; // Definiendo la variable true 
+console.log("typeof value [" + typeof(value2) + "]"); // evaluando que tipo de dato es la variable true
+value2 = String(value2);  // cambiando la variable true a tipo de dato String 
+console.log("typeof value [" + typeof(value2) + "]");
+value2 = Number(value2);  // cambiando la variable true a tipo de dato Number
+console.log("typeof value [" + typeof(value) + "]");
+
+let value3 = 0; // Definiendo la variable 0
+console.log("typeof value [" + typeof(value3) + "]"); // evaluando que tipo de dato es la variable 0
+value3 = String(value3);  // cambiando la variable 0 a tipo de dato String 
+console.log("typeof value [" + typeof(value3) + "]");
+value3 = Boolean(value3); // cambiando la variable 0 a tipo de dato Boolean
+console.log("typeof value [" + typeof(value3) + "]");
+
+
+
+
 
 
