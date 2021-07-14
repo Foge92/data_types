0b8f812d5dbfb6336eaa0f97c224a1266175f026
Elias Flores Vazquez <eliasfv25@gmail.com>
2021-07-14 00:26:09 -0500

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+//1.1
+let marcaAuto ="AUDI";
+console.log(marcaAuto);
+//1.2
+let edad;
+console.log(edad);
+//1.3
+let nombreCondutor='hacker1';
+console.log(nombreCondutor);
+//1.4
+console.log("El nombre del conductor es " +nombreCondutor);
+
+//1.5
+let numUno = 38, numDos = 14;
+console.log("La suma de " + numUno +" + " + numDos + " = " + (numUno+numDos)); 
+
+//1.6
+let name = "Elias";
+let hobby = "programar";
+let pets = "gata";
+console.log("Hola soy "+ name + ". Me gusta " + hobby +" en mis tiempos libres y tengo un " + pets +" como mascota");
