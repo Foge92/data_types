95ad9ce90cfdfb88c738b96a1aa1714bcbb2a23c
Erika Diaz <ediazbuceta@gmail.com>
2021-07-13 23:29:39 -0500

    Primera parte de la tarea

---
+++ datos.js
@@ -0,0 +1,26 @@
+let marcaAuto = "Mercedes-Benz"  // variable que almacena la marca de un automovil
+let edad = 24;                   // variable que almacena la edad
+let hacker1 = "Erika"            //variable que almacena el nombre del conductor
+
+console.log("El nombre del conductor es: " + hacker1); 
+
+let numUno = 38;
+let numDos = 14;
+let resultado = numUno + numDos;  //suma de las variables numUno y numDos
+console.log(resultado);           //muestra la suma de los dos numeros 
+
+//Tarjeta de presentacion pidiendo los datos al usuario.
+nombre = window.prompt("Dime tu nombre","");
+hobby = window.prompt("Dime tus hobbies","");
+gusto = window.prompt("Dime 3 cosas que te gusten?","");
+mascota = window.prompt("Que mascota tiene?","");
+console.log("Hola, yo soy " + nombre + ", mis hobbies son " + hobby + ". Me gusta " + gusto + ". Tengo " + mascota + " como mascota." );
+
+//Tarjeta de presentacion con los valores ya definidos
+let nombre2 = "Erika"
+let hobby2 = "ver peliculas de accion, jugar con mi sobrinito e investigar sobre grupos musicales"
+let gusto2 = "La musica, el arte y dormir"
+let mascota2 = "dos perritos"
+console.log("Hola, yo soy " + nombre2 + ", mis hobbies son " + hobby2 +". Me gusta " + gusto2 + ". Tengo " + mascota2 + " como mascotas.");
+
+
