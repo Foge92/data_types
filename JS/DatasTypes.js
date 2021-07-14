0a9222cc885beaff4b7a3b9e4686729b817c4a8e
ISRAEL HERNANDEZ VAZQUEZ <al2183045545@azc.uam.mx>
2021-07-13 20:04:29 -0500

    feat | Cree punto 1.2

--- JS/DatasTypes.js
+++ JS/DatasTypes.js
@@ -1,3 +1,7 @@
 /*1.1 Crea una variable que contenga la marca de un automóvil*/
 let marcaAutomovil="Volkswagen";
 console.log(marcaAutomovil);
+/*1.2 Crea un variable para almacenar la edad*/
+let edad;
+edad=24;
+console.log(edad);
