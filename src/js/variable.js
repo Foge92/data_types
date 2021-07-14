52ff34a6a0f92718acd05d73d5e41aad9a32cf74
Jose Antonio <joseanttorres28@gmail.com>
2021-07-13 22:20:31 -0500

    done

---
+++ src/js/variable.js
@@ -0,0 +1,18 @@
+//Marca de mi auto, string
+let marca="Mercedes-Benz";
+console.log("La marca de mi auto:"+marca);
+//Mi edad, number
+let edad=23;
+console.log("Mi edad es: "+edad);
+//Nombre del conductor, string
+let hacker1="José Antonio";
+console.log("El nombre del conductor es "+hacker1);
+//Suma de dos numeros
+let numUno=38;
+let numDos=14;
+console.log("La suma de "+numUno+" y "+numDos+" es "+(numDos+numUno));
+//Tarjeta de presentación
+let nombre="José Antonio";
+let hobby="Ver Anime";
+let mascota="Perro";
+console.log("Hola soy "+nombre+". Me gusta "+hobby+" en mis tiempos libres y tengo un "+mascota+" como mascota.")
