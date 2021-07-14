95541f9b3c99f35ef60660ce7a099dc1d1f39279
ISRAEL HERNANDEZ VAZQUEZ <al2183045545@azc.uam.mx>
2021-07-13 19:56:41 -0500

    feat | actualice data_types

--- JS/DatasTypes.js
+++ JS/DatasTypes.js
@@ -1,5 +1,3 @@
-/*
-1.1 Crea una variable que contenga la marca de un automóvil
-*/
+/*1.1 Crea una variable que contenga la marca de un automóvil*/
 let marcaAutomovil="Volkswagen";
 console.log(marcaAutomovil);
