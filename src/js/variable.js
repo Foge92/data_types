376be1880789ef11ce1e5a5a0e9b3f870f879e51
Jose Antonio <joseanttorres28@gmail.com>
2021-07-14 01:56:49 -0500

    bonus-1

--- src/js/variable.js
+++ src/js/variable.js
@@ -23,3 +23,29 @@ console.log(String(valor));
 console.log(Boolean(valor));
 }
 conversion(undefined);
+
+//Bonus1
+let contenedor="Lorem ipsum      dolor sit amet, consectetur adipiscing elit. Integer hendrerit lectus varius sapien aliquam mattis. Suspendisse laoreet libero quis ligula finibus, eget consequat lorem dapibus. Aliquam quis viverra purus, a pharetra augue. Maecenas tincidunt in dolor at auctor. Morbi est nunc, feugiat quis ipsum id, pharetra efficitur quam. In in libero sit amet dui varius vestibulum a tempor nunc. Maecenas rhoncus augue vitae erat facilisis, sit amet cursus purus luctus. Nullam vel felis odio. Etiam lobortis pharetra enim, vel hendrerit metus tempus placerat. Vestibulum imperdiet ligula nisi, ut lacinia sem venenatis ac. Sed pretium mi a tellus commodo, eget ultricies ligula facilisis. Maecenas semper tristique enim quis vulputate. Nulla vitae massa sem. Fusce sollicitudin elit accumsan pellentesque accumsan. Nam pellentesque sem massa, nec vestibulum ante aliquet ac. Ut libero nisl, tempor ut feugiat non, sodales eu leo.\nNulla eu suscipit purus. Aenean suscipit nulla id rutrum imperdiet. Morbi vitae mauris porttitor augue blandit egestas. Etiam in odio mi. Donec mi ante, hendrerit sit amet sapien nec, hendrerit interdum diam. Nulla ante arcu, euismod quis hendrerit eget, lacinia id libero. In auctor leo nisl, ut pulvinar ipsum cursus eu. Sed sit amet augue tristique, interdum risus sed, ornare ligula. In non sagittis arcu. Etiam consectetur consectetur iaculis. Sed convallis sed magna quis convallis. In sed vehicula sem, in aliquet magna. In feugiat justo leo, et maximus lectus faucibus sed. Cras tortor ante, fermentum eget consectetur fermentum, gravida ut nulla. Pellentesque feugiat auctor leo.\nPhasellus scelerisque pharetra nisl sollicitudin ultricies. Praesent luctus felis eu semper placerat. Nunc consequat egestas sapien, vitae sagittis ex fermentum quis. Mauris sed turpis posuere, eleifend dui tempor, egestas justo. Curabitur ac tortor vestibulum, gravida mauris a, varius tortor. Aliquam sit amet elit pretium, mattis leo venenatis, elementum quam. Integer a eros sodales eros venenatis viverra. Praesent scelerisque et diam vitae vestibulum. Aliquam erat volutpat. Duis quis ipsum vitae ligula dignissim aliquet et sit amet nunc. Cras commodo lorem tellus. Duis pretium at enim non egestas. Ut lectus augue, malesuada id aliquam ac, malesuada ut arcu.";
+//Función para contar numero de palabras
+function contar(parrafo){
+    let cadena;
+    //console.log(parrafo.split(/\s+/));
+    //Split, para crear un arreglo de subcadenas del string, indicado por un patron en este caso
+    //los espacios en blanco
+    cadena=parrafo.split(/\s+/);
+    return cadena.length;
+}
+//Función para contar las veces que se encuentra et en la cadena
+function buscaret(parrafo){
+    let cadena;
+    //Split, para crear un arreglo de subcadenas del string, indicado por un patron en este caso
+    //los espacios en blanco
+    cadena=parrafo.split("et");
+    //Como la ultima subcadena no contiene et, se le resta un uno.
+    return cadena.length-1;
+}
+console.log("El numero de palabras es "+contar(contenedor));
+console.log("Las veces que se repite et es: "+buscaret(contenedor));
+
+
+
