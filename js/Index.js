8966ec5b5cdcf15174b792fc6bab13f2b76db0fe
Cesar <cesaradrianer2@gmail.com>
2021-07-13 21:43:25 -0500

    Iteración 1 | completada

---
+++ js/Index.js
@@ -0,0 +1,13 @@
+let Chevrolet;
+let edad = 30;
+let hacker1 = "Roberto";
+console.log("El nombre del conductor es " + hacker1);
+let numUno = 38;
+let numDos = 14;
+console.log(numUno + numDos);
+var tarjeta = {
+    nombre: 'César',
+    hobby: 'la música',
+    mascota: "perro"
+}
+console.log("Hola soy " + tarjeta.nombre + ". Me gusta " + tarjeta.hobby + " en mis tiempos libres y tengo un " + tarjeta.mascota + " como mascota")
